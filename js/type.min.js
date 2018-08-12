27a236a13c222bac5711d4596e33bbcb8a729b70
Lincoln Lo <Lolincoln23@gmail.com>
2018-08-12 00:46:40 -0400

    Website ready to use

---
+++ js/type.min.js
@@ -0,0 +1,7 @@
+  var typed3 = new Typed('#typed', {
+    strings: ['I am a: <strong>Software Developer...</strong>', 'I am a: <strong>Marathon Runner...</strong>', 'I am a: <strong>Student Studying Computer Engineering...</strong>', 'I am a: <strong>Student At The University of Waterloo.</strong>'],
+    typeSpeed: 35,
+    backSpeed: 35,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
