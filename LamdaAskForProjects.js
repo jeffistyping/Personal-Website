99e035d6c7291bff82074e836d9eb40c5c4cb279
Lincoln Lo <Lolincoln23@gmail.com>
2018-08-21 00:22:28 -0400

    changes to lamda function

--- LamdaAskForProjects.js
+++ LamdaAskForProjects.js
@@ -1,7 +1,6 @@
 exports.handler = (event, context, callback) => {
 
     event.sessionAttributes['repeat'] = true;
-    console.log("This is the session attribute" + event.sessionAttributes['repeat']);
     //Get the slot values and set them to variables
     let userInput = event.inputTranscript;
     let LanguageType = event.currentIntent.slots.Language;
@@ -12,6 +11,7 @@ exports.handler = (event, context, callback) => {
     };
 
     console.log("THIS IS EVENT: " + JSON.stringify(event));
+    console.log("THIS IS CONTEXT: " + JSON.stringify(context));
 
     //This function sends messages to the user. The message type is what distinguishes the various kinds of messages
     function sendUserMessage(type, message, intentName, slots, slotToElicit, fulfillmentState) {
@@ -76,7 +76,7 @@ exports.handler = (event, context, callback) => {
                 callback(null, {
                     "dialogAction": {
                         "type": "Delegate",
-                        "slots": slots
+                        "slots": slots,
                     }
                 });
                 break;
@@ -84,26 +84,35 @@ exports.handler = (event, context, callback) => {
         }
     }
 
+    function languageValidate(input) {
+        let matchLanguage = new RegExp('javascript|node\\.js|node|HTML|html|css|CSS|Bootstrap|bootstrap|Java|java|C\\+\\+|c\\+\\+|Android|android|PHP|php');
+        let match = matchLanguage.exec(input);
+        if (match == null) {
+            return null;
+        } else {
+            return "success";
+        }
+    }
 
     if (userInput == "reset" || userInput == "cancel") {
-        var messageToSend = "Okay, conversation has been reset. Is there anything I can help you with?";
-        sendUserMessage("ElicitIntent", messageToSend, null, null, null, null);
+        sendUserMessage("Delegate", null, null, slots, null, null);
     }
-    let repeat = true;
-    if (LanguageType == null && event.sessionAttributes['repeat']) {
-        let response = "I have done lots of projects! Ask me about a language that you want to learn more about!"
+
+    if (event.sessionAttributes['repeat'] && LanguageType == null) {
+        let response = "I have done lots of projects! Discover some of my project by asking me a particular programming language";
         sendUserMessage("ElicitSlot", response, "Projects", slots, "Language", null);
-    } else {
-        if (LanguageType == "node" || LanguageType == "javascript" || LanguageType == "node.js") {
-            console.log("in Node");
-            let response = "I have built a Data-Integration software which allow multiple users to automatically pull data from 10+ services like SalesForce, Quickbooks, Xero and gather all the information into a single database which can then be analyze. I have also built a system to allow for users to seamlessly Authorize with Oauth2.0."
+    }else {
+        let string = event.inputTranscript
+        console.log(string);
+        console.log("See if comparison works"+ string.includes("node"));
+        if(string.includes("node")){
+            LanguageType = "node"
+            let response = "I have built a Data-Integration software which allow multiple users to automatically pull data from 10+ services like SalesForce, Quickbooks, Xero and gather all the information into a single database which can then be analyze. I have also built a system to allow for users to seamlessly Authorize with Oauth2.0. \n\n Ask me about another language or quit by typing cancel"
             event.sessionAttributes['repeat'] = false;
-            console.log("In here should be false now " + event.sessionAttributes['repeat']);
             sendUserMessage("ElicitSlot", response, "Projects", slots, "Language", null);
         } else {
             let userMessage = "Sorry, Looks like I haven't done a project with that language yet! However, Please feel free to checkout my github for all the other projects: https://github.com/Lincoln23 ";
             sendUserMessage("Close", userMessage, "Projects", slots, "Language", null);
         }
     }
-
-};
+};     
