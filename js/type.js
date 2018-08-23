5a0917f719c2244ff92fc225069653434cd4bbdd
Lincoln Lo <Lolincoln23@gmail.com>
2018-08-23 18:39:31 -0400

    Added .min to compress code

--- js/type.js
+++ js/type.js
@@ -1,7 +1,7 @@
-  var typed3 = new Typed('#typed', {
-    strings: ['I am a: <strong>Software Developer...</strong>', 'I am a: <strong>Marathon Runner...</strong>', 'I am a: <strong>Student Studying Computer Engineering...</strong>', 'I am a: <strong>Student At The University of Waterloo.</strong>'],
-    typeSpeed: 35,
-    backSpeed: 35,
-    smartBackspace: true, // this is a default
-    loop: true
-  });
+let typed3 = new Typed('#typed', {
+  strings: ['I am a: <strong>Software Developer...</strong>', 'I am a: <strong>Marathon Runner...</strong>', 'I am a: <strong>Student Studying Computer Engineering...</strong>', 'I am a: <strong>Student At The University of Waterloo.</strong>'],
+  typeSpeed: 40,
+  backSpeed: 40,
+  smartBackspace: !0,
+  loop: !0
+})
